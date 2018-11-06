d98c2c390bd90644d6684a98babeb6f742c5517b
Nils Wloka <nils.wloka@codecentric.de>
2018-11-06 07:56:22 +0100

    Read latest value from one time series.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: {}
+    };
+  }
+
+  async componentDidMount() {
+    await this.loadData();
+  }
+
+  loadData = async () => {
+    const baseUrl = '/api/iottimeseries/v3/timeseries/';
+    const deviceId = '1310450cbc204d238c8f4662987aa01f';
+    const dataSet = 'ambientLight';
+    const result = await axios.get(baseUrl + deviceId + '/' + dataSet, {
+      xrsfCookieName: 'x-xsrf-token',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).catch(console.log);
+    this.setState({ data: result.data });
+    console.log(result);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{JSON.stringify(this.state.data, null, 2)}</p>
       </div>
     );
   }
